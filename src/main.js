2eef9576eae06abb9b9c5ccce2c0a3071e7e580a
ColeyG <cjgeerts@gmail.com>
2019-11-25 16:29:22 -0500

    Init fetch

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 import '../styles/reset.css';
 import '../styles/main.scss';
 import 'babel-polyfill';
+
+async function fetchData() {
+  await fetch('http://localhost:3000/api', { method: 'GET', mode: 'no-cors' })
+    .then((resp) => {
+      console.log(resp);
+      resp.text();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+const initialize = () => {
+  fetchData();
+};
+
+initialize();
